5bdb7bfa4d558059739e5b396200aae413b4209d
Kishan Rekhadia <kishanrekhadia79@gmail.com>
2023-06-03 14:36:40 +0530

    Added props children plus form for meetup

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
-
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" exact Component={AllMeetupsPage} />
-        <Route path="/new-meetup" Component={NewMeetupPage} />
-        <Route path="/favorites" Component={FavoritesPage} />
-      </Routes>
-      
-    </div>
+    <Layout>
+      <div>
+        <Routes>
+          <Route path="/" exact Component={AllMeetupsPage} />
+          <Route path="/new-meetup" Component={NewMeetupPage} />
+          <Route path="/favorites" Component={FavoritesPage} />
+        </Routes>
+      </div>
+    </Layout>
   );
 }
 
