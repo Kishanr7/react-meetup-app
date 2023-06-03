90aac242313d841e452e029ca2f5273dfc3bf8fa
Kishan Rekhadia <kishanrekhadia79@gmail.com>
2023-06-03 18:22:49 +0530

    Adding input handler for forms + POST requests to store data in firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://playground-6205f-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>;
 }
 
